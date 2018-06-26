55d81921349ce9f2f5610f00cbccea95ff1fcc9d
U-OKBHMAO\kve <kve1989@gmail.com>
2018-06-26 09:29:58 +0500

    Fix screenshooter.sh

--- scripts/screenshooter.sh
+++ scripts/screenshooter.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-filename=screenshot-%d-%m-%y-%T.png
+filename=screenshot-%d-%m-%y-%T.jpg
 
 notify-send "Scrot" "$filename" -i /usr/share/icons/Adwaita/48x48/devices/camera-photo.png
 scrot ~/Pictures/Screenshots/$filename
