8dfa555f6841e1f28dfa03fbad7781f27817e230
Vadim Kavetskiy <33026354+kve1989@users.noreply.github.com>
2022-01-17 21:34:45 +0500

    Create user_profile.ps1

---
+++ .config/powershell/user_profile.ps1
@@ -0,0 +1,30 @@
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-PoshPrompt Appendix
+
+Import-Module Terminal-Icons
+
+# PSReadLine
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadLineOption -PredictionSource History
+
+# Fzf
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+# Alias
+Set-Alias ll ls
+Set-Alias grep findstr
+
+# Utilities
+function which ($command) {
+  Get-Command -Name $command -ErrorAction SilentlyContinue |
+    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
+
+function take ($folder) {
+    mkdir $folder
+    cd $folder
+}
